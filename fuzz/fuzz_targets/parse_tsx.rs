8129773ddc9637f74abbcf0ccb8d082f220f0562
derekirish3 <lcfisch2@asu.edu>
2023-05-31 14:58:55 -0700

    Mayhem Support

---
+++ fuzz/fuzz_targets/parse_tsx.rs
@@ -0,0 +1,13 @@
+use honggfuzz::fuzz;
+use tree_sitter::Parser;
+
+fn main() {
+    loop {
+        fuzz!(|data: &[u8]| {
+            let code: &str = std::str::from_utf8(data).unwrap();
+            let mut parser = Parser::new();
+            parser.set_language(tree_sitter_typescript::language_tsx()).expect("Error loading tsx grammar");
+            let _ = parser.parse(code, None);
+        });
+    }
+}
